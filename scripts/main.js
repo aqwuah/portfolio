24ece8a6cba1200a09290ae9d0be08764cf647da
aqwuah <thomas.e1300@gmail.com>
2022-10-30 10:54:28 +0000

    finished commands and changed design

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,11 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 app.addEventListener("keypress", async function(event){
   if(event.key === "Enter")
   {
-    await delay(150); // small delay between pressing enter and command running
+//  await delay(150);
     getInputValue();
    
     removeInput();
-    await delay(150); // more delays
+//  await delay(150);
     new_line();
   }
 });
@@ -40,11 +40,17 @@ async function open_terminal() // sends opening lines of text
 async function close_terminal() // removes the terminal from the screen
 {
   document.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("span1").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("span2").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("div").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("i").forEach(e => e.parentNode.removeChild(e));
+  await delay(50);
   document.querySelectorAll("input").forEach(e => e.parentNode.removeChild(e));
 }
 
@@ -75,7 +81,7 @@ function new_line() // creates a new line with info and a pointer (>), and focus
   const div = document.createElement("div");
   div.setAttribute("class", "type")
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icon")
   const input = document.createElement("input");
   div.appendChild(i);
   div.appendChild(input);
@@ -99,7 +105,7 @@ async function getInputValue() // check what the user typed and show output
   {
     trueValue(value); 
     createCommand("about me", "a little bit about what i do");
-    createCommand("social", "my social networks");
+    createCommand("social -a/--all", "all my social networks");
     createCommand("skills", "things i know")
     createCommand("projects", "everything i have worked on");
   }
@@ -122,11 +128,12 @@ async function getInputValue() // check what the user typed and show output
     createText("--- games ---")
     createText("<a href='https://runner.aqwuah.com'><u>runner</u></a> - unoriginal (not endless) runner i made in like 5 mins using unity")
   }
+
   else if (value === "about me")
   {
     trueValue(value);
     createText("hey, i'm aqua :D")
-    createText("i spent alot of time doing anything tech related. i code, create bugs, play games, attempt to create games, fix tech, break tech etc. as well as this, i play the drums and piano.")
+    createText("i spent alot of time doing anything tech related. i code, create bugs, play games, attempt to create games, fix tech, break tech etc. as well as this, i play the drums and piano, and enjoy helping others with coding.")
   }
 
   else if (value === "social -a" || value === "social --all")
@@ -137,10 +144,41 @@ async function getInputValue() // check what the user typed and show output
     createText("<a href='https://discordapp.com/users/835582617323044914' target='_blank'><i class='fab fa-discord white'></i> aqua#5664 (discord)")
   }
 
-  else if (value === "social") // for if user forgets the flag
+  else if (value === "skills")
+  {
+    trueValue(value);
+    createText("<a id='top-of-skills'>i spent most of my time dipping into random projects, and that means i have knowledge in lots of different things.</a>")
+    createText("--- languages ---")
+    createText("python - first language i learnt and i am now advanced in it, and use it all the time")
+    createText("sql - used with python and am fairly advanced in accessing databases")
+    createText("html, css - learnt these from a young age and am proficient in them")
+    createText("js - learnt alongside the markup languages and am confident using it")
+    createText("c - learning from harvard university, intermediate knowledge")
+    createText("c# - working on game development and consequentially c#, basic knowledge")
+    createText("rust - learning")
+    createText("--- version control ---")
+    createText("git - confident using a cli for version control")
+    createText("--- ethical hacking ---")
+    createText("networking - subnetting, the osi model etc")
+    createText("osint - confident")
+    createText("hacking skills - proficient and still learning")
+    createText("social engineering - learning")
+    createText("--- misc ---")
+    createText("cyber security - confident in defence and offense (red + blue teams)")
+    createText("game development - learning 2d and 3d development")
+    createText("<a href='#top-of-skills'><u>(click to scroll to top)</u></a>")
+  }
+
+  else if (value === "social") // for if user forgets the -a / --all
   {
     trueValue(value);
     createText("Didn't you mean: social -a/--all?")
+  }
+
+  else if (value === "social -a/--all" || value === "social -a --all" || value === "social --all -a") // for if user does both -a and --all
+  {
+    trueValue(value);
+    createText("You can use either -a or --all, not both!")
   }
 
   else if (value === "sudo rm -rf")
@@ -151,12 +189,19 @@ async function getInputValue() // check what the user typed and show output
     explosion_art();
     await delay(4000)
     close_terminal();
+    await delay(2000);
+    destroyed_alert();
   }
   
   else if (value === "clear") // removing all previous text
   {
     document.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e)); 
     document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
+  }
+
+  else if (value === "") // user just presses enter
+  {
+    // does nothing, just makes a new line
   }
 
   else
@@ -166,14 +211,19 @@ async function getInputValue() // check what the user typed and show output
   }
 }
 
+function destroyed_alert()
+{
+  alert("The website has been destroyed. Don't say I didn't warn you.")
+}
+
 function trueValue(value) // makes the pointer green
 {
   const div = document.createElement("section");
   div.setAttribute("class", "type2")
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icon success")
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "sucess")
+  mensagem.setAttribute("class", "success")
   mensagem.textContent = `${value}`;
   div.appendChild(i);
   div.appendChild(mensagem);
@@ -185,7 +235,7 @@ function falseValue(value) // makes the pointer red
   const div = document.createElement("section");
   div.setAttribute("class", "type2")
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone error")
+  i.setAttribute("class", "fas fa-angle-right icon error")
   const mensagem = document.createElement("h2");
   mensagem.setAttribute("class", "error")
   mensagem.textContent = `${value}`;
